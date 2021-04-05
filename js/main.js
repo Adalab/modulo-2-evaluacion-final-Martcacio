b6e5ec1c2d64ba98b4352681d9e414f4ca7b07c1
Marta Cano <martanchor@gmail.com>
2021-04-05 11:43:48 +0200

    updated buttons delete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 let series = [];
 let favs = [];
@@ -9,15 +9,15 @@ function getDataFromApi(searchValue) {
         .then((response) => response.json())
         .then((data) => {
             series = data;
-            renderSeries()
-        })
+            renderSeries();
+        });
     setFavs();
-};
+}
 
 //Search
-const button = document.querySelector('.search-button-js');
-const inputElement = document.querySelector('.search-input-js');
-const formElement = document.querySelector('.form-js');
+const button = document.querySelector(".search-button-js");
+const inputElement = document.querySelector(".search-input-js");
+const formElement = document.querySelector(".form-js");
 
 function handleSubmit(ev) {
     ev.preventDefault();
@@ -27,21 +27,20 @@ function handleSearch() {
     getDataFromApi(inputElement.value);
     setFavs();
 }
-formElement.addEventListener('submit', handleSubmit);
-button.addEventListener('click', handleSearch);
+formElement.addEventListener("submit", handleSubmit);
+button.addEventListener("click", handleSearch);
 
 //Render
 function renderSeries() {
-    let htmlCode = '';
+    let htmlCode = "";
 
     for (const serie of series) {
-
-        let serieImage = '';
+        let serieImage = "";
         if (serie.show.image === null) {
-            serieImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+            serieImage = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
         } else {
             serieImage = serie.show.image.medium;
-        };
+        }
 
         if (isFav(serie)) {
             htmlCode += `<li class="serie-card fav serie-card-js" id="${serie.show.id}">`;
@@ -50,15 +49,16 @@ function renderSeries() {
         }
         htmlCode += `<img class="img" src="${serieImage}" class="serie-img">`;
         htmlCode += `<p class="serie-name">${serie.show.name}</p>`;
+        htmlCode += `<p class="serie-status">${serie.show.status}</p>`;
         htmlCode += ` </li>`;
     }
-    const seriesContainer = document.querySelector('.container-js');
+    const seriesContainer = document.querySelector(".container-js");
 
     seriesContainer.innerHTML = htmlCode;
 
-    const serieItems = document.querySelectorAll('.serie-card-js');
+    const serieItems = document.querySelectorAll(".serie-card-js");
     for (const serieItem of serieItems) {
-        serieItem.addEventListener('click', handleSerie);
+        serieItem.addEventListener("click", handleSerie);
     }
     setFavs();
 }
@@ -69,12 +69,11 @@ function isFav(serie) {
         return fav.show.id === serie.show.id;
     });
     if (favFound === undefined) {
-        return false
+        return false;
     } else {
-        return true
-    };
+        return true;
+    }
 }
-
 
 //Listen series
 function handleSerie(ev) {
@@ -99,49 +98,88 @@ function handleSerie(ev) {
     setFavs();
     renderSeries();
     renderFavSeries();
-};
+}
 
-
-
-//Mis favs 
+//Mis favs
 function renderFavSeries() {
-    let htmlCode = '';
+    let htmlCode = "";
 
     for (const fav of favs) {
-
-        let favImage = '';
+        let favImage = "";
         if (fav.show.image === null) {
-            favImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+            favImage = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
         } else {
             favImage = fav.show.image.medium;
-        };
-
+        }
         htmlCode += `<li class="seriefav-card fav seriefav-card-js" id="${fav.show.id}">`;
         htmlCode += `<img class="img-fav" src="${favImage}" class="serie-img">`;
         htmlCode += `<p class="serie-name">${fav.show.name}</p>`;
+        htmlCode += `<button class="button-cross cross-js" id="${fav.show.id}"><i class="fas fa-backspace"></i></button>`;
         htmlCode += ` </li>`;
     }
-    const seriesFavContainer = document.querySelector('.container-fav-js');
+    const seriesFavContainer = document.querySelector(".container-fav-js");
 
     seriesFavContainer.innerHTML = htmlCode;
+    removeCrossButton();
 }
 
+//Delete all
+function deleteAll() {
+    const deleteAll = document.querySelector(".delete-js");
+    deleteAll.addEventListener("click", handleDelete);
+}
+deleteAll();
+
+function handleDelete() {
+    localStorage.removeItem("favs");
+    favs = [];
+    renderSeries();
+    renderFavSeries();
+}
+
+//Remove x
+function removeCrossButton() {
+    const removeCrosses = document.querySelectorAll(".cross-js");
+    localStorage.removeItem("favs");
+    for (const removeCross of removeCrosses) {
+        removeCross.addEventListener("click", handleRemoveCross);
+    }
+}
+
+function handleRemoveCross(ev) {
+    const clickedRemoveId = parseInt(ev.currentTarget.id);
+    const deleteIndex = favs.findIndex((fav) => fav.show.id === clickedRemoveId);
+    if (deleteIndex !== -1) {
+        favs.splice(deleteIndex, 1);
+        renderFavSeries();
+        renderSeries();
+    }
+}
 
 //Local Storage
 function setFavs() {
     const stringyFavs = JSON.stringify(favs);
-    localStorage.setItem('favs', stringyFavs);
-};
-
+    localStorage.setItem("favs", stringyFavs);
+}
 
 function getFavs() {
-    const favsStorage = localStorage.getItem('favs');
+    const favsStorage = localStorage.getItem("favs");
     if (favsStorage !== null) {
         favs = JSON.parse(favsStorage);
         renderFavSeries();
     }
-};
-
+}
 
 getFavs();
-getDataFromApi('girls');
+getDataFromApi("girls");
+
+//Log
+const logFavs = document.querySelector(".log-js");
+
+function handleLogFavs() {
+    for (const fav of favs) {
+        console.log(fav.show.name);
+    }
+}
+
+logFavs.addEventListener("click", handleLogFavs);
